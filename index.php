25f694aa5cad4f67625643b0aeb2b2386843addf
vagrant <vagrant@homestead>
2019-01-03 16:35:12 +0000

    Clase: Agregar archivos extrenos

--- index.php
+++ index.php
@@ -1,69 +1,10 @@
 <?php 
 
+require_once('jobs.php');
+
 $lastName = 'Soto';
 $name = "Farid $lastName";
 $limitMonths = 12;
-$jobs = [
-  [
-    'title' => 'PHP Developer',
-    'description' => 'This is an awesome job',
-    'visible' => true,
-    'months' => 6
-  ],
-  [
-    'title' => 'Javascript',
-    'description' => 'This is an awesome job',
-    'visible' => true,
-    'months' => 4
-  ],
-  [
-    'title' => 'Devops',
-    'description' => 'This is an awesome job',
-    'visible' => false,
-    'months' => 3
-  ],
-  [
-    'title' => 'Node Dev',
-    'description' => 'This is an awesome job',
-    'visible' => true,
-    'months' => 5
-  ],
-  [
-    'title' => 'Frontend Dev',
-    'description' => 'This is an awesome job',
-    'visible' => false,
-    'months' => 7
-  ],
-];
-
-function getDuration($months){
-
-  $years = floor($months / 12);
-  $extraMonths = $months % 12;
-
-  return "$years years $extraMonths months";
-}
-
-function printJob($job) {
-  //Contenido de la Funcion 
-  if($job['visible'] == false){
-    return;
-  }
-
-  echo '<li class="work-position">';
-  echo '<h5>'. $job['title'] . '</h5>';
-  echo '<p>'. $job['description'] . '</p>';
-  echo '<p>'. getDuration($job['months']) .'</p>';
-  //echo '<p>'. $totalMonths . '</p>';
-  echo '<strong>Achievements:</strong>';
-  echo '<ul>';
-  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
-  echo '</ul>';
-  echo '</li>';
-}
-
 
 
 ?>
---
+++ jobs.php
@@ -0,0 +1,33 @@
+<?php
+$jobs = [
+    [
+      'title' => 'PHP Developer',
+      'description' => 'This is an awesome job',
+      'visible' => true,
+      'months' => 6
+    ],
+    [
+      'title' => 'Javascript',
+      'description' => 'This is an awesome job',
+      'visible' => true,
+      'months' => 4
+    ],
+    [
+      'title' => 'Devops',
+      'description' => 'This is an awesome job',
+      'visible' => false,
+      'months' => 3
+    ],
+    [
+      'title' => 'Node Dev',
+      'description' => 'This is an awesome job',
+      'visible' => true,
+      'months' => 5
+    ],
+    [
+      'title' => 'Frontend Dev',
+      'description' => 'This is an awesome job',
+      'visible' => false,
+      'months' => 7
+    ],
+  ];
