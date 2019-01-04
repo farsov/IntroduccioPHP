2f30a0666a5bbab4c6329ed9bd9d40525476f8ed
vagrant <vagrant@homestead>
2019-01-04 19:54:57 +0000

    Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,14 +1,11 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
-class Job extends BaseElement  {
+class Job extends Model  {
 
-    public function __construct($title, $description) {
-        $newTitle = 'Job: ' . $title;
-        //parent::__construct($newTitle, $description); // acceder al constructor padre
-        $this->title = $newTitle;
-    }
+    protected $table = 'jobs';
 
     public function getDurationAsString(){
         $years = floor($this->months / 12);
--- jobs.php
+++ jobs.php
@@ -3,37 +3,24 @@
 
 require_once 'vendor/autoload.php';
 
-use App\Models\{Job, Project, Printable};
+use App\Models\{Job, Project};
 
-$job1 = new Job('PHP Developer', 'This is an awesome job!!!');
-$job1->months = 16;
-
-$job2 = new Job('Python Developer', 'This is an awesome job!!!');
-$job2->months = 24;
-
-$job3 = new Job('Devops', 'This is an awesome job!!!');
-$job3->months = 32;
+$jobs = Job::all();
 
 $project1 = new Project('Project 1', 'Description 1');
 
-
-$jobs = [
-    $job1,
-    $job2,
-    $job3
-  ];
 $projects = [
     $project1
 ];
   
-function printElement(Printable $job) {
-    if($job->visible == false) {
+function printElement( $job) {
+    /*if($job->visible == false) {
       return;
-    }
+    }*/
   
     echo '<li class="work-position">';
-    echo '<h5>' . $job->getTitle() . '</h5>';
-    echo '<p>' . $job->getDescription() . '</p>';
+    echo '<h5>' . $job->title . '</h5>';
+    echo '<p>' . $job->description . '</p>';
     echo '<p>' . $job->getDurationAsString() . '</p>';
     echo '<strong>Achievements:</strong>';
     echo '<ul>';
