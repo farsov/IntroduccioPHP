16a821a3ea40b02a694d06261537b06292797215
vagrant <vagrant@homestead>
2019-01-04 20:12:48 +0000

    FrontController

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+// Front Controller
+
+ini_set('display_errors', 1);
+ini_set('display_starup_error', 1);
+error_reporting(E_ALL);
+
+require_once '../vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'cursophp',
+    'username'  => 'homestead',
+    'password'  => 'secret',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+/* Obtener la ruta */
+$route = $_GET['route'] ?? '/';
+
+if($route == '/') {
+    require '../index.php';
+} elseif($route == 'addJob') {
+    require '../addJob.php';
+}
+
+require '../index.php';
+
